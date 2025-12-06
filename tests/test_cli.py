4028093b50c69cb98d2f157f8dbff85569f9825f
giles knap <giles.knap@diamond.ac.uk>
2025-12-06 17:11:57 +0000

    fix tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -6,4 +6,4 @@ from awusb import __version__
 
 def test_cli_version():
     cmd = [sys.executable, "-m", "awusb", "--version"]
-    assert subprocess.check_output(cmd).decode().strip() == __version__
+    assert subprocess.check_output(cmd).decode().strip() == f"awusb {__version__}"
